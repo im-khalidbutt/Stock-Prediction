c2168f1dcfd6c7163fd2f80ddd023824b3d7488d
Khalid Butt <khalidbutt94@gmail.com>
2025-06-16 17:24:03 +0500

    jwt implementation, login and and logout page

--- stock_frontend/src/components/Header.jsx
+++ stock_frontend/src/components/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Button from './button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
+
+
 const Header = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  const  navigate = useNavigate()
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    navigate('/login')
+  }
   return (
     <>
       <nav className="navbar bg-body-tertiary ">
       <div className="container ">
         <Link className="navbar-brand" to={'/'} >Stock</Link>
-        <div>
-        <Button text="Login" class="btn-outline-primary"  url='/login'/>
-        &nbsp; 
-        <Button text="Sign In" class="btn-outline-success" url='/register'/>
+        <div> {isLoggedIn ? (<button className='btn btn-danger' onClick={handleLogout}>Logout</button>) : (
+            <>
+            <Button text="Login" class="btn-outline-primary"  url='/login'/>
+              &nbsp; 
+            <Button text="Sign In" class="btn-outline-success" url='/register'/>
+            </>
+        ) }
         </div>
       </div>
     </nav>
